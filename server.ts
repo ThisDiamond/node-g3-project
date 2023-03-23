0ca000e2a4e7ab1768032ec71b9b5696eec3ac65
DilshodbekNodeJs <DilshodbekNodejs@gmail.com>
2023-03-23 10:24:00 +0500

    template

--- server.ts
+++ server.ts
@@ -8,6 +8,11 @@ server.use(cors());
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
+server.engine("hbs", engine({ extname: ".hbs" }));
+server.set("view engine", "hbs");
+server.set("views", "./views");
+
+server.get("/", (req, res) => {});
 const PORT = process.env.PORT || 1112;
 server.listen(PORT, () => console.log(`server running on port  ${PORT}`));
 
