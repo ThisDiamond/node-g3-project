5762b16b1d0a5882f207afc164ec450199231e65
DilshodbekNodeJs <DilshodbekNodejs@gmail.com>
2023-03-25 12:30:15 +0500

    posts

--- server.ts
+++ server.ts
@@ -1,9 +1,12 @@
 import express, { Express } from "express";
 import cors from "cors";
 import { engine } from "express-handlebars";
+import sqlite3 from "sqlite3";
 
 const server: Express = express();
+const db = new sqlite3.Database("./post.db");
 
+server.use(express.static("public"));
 server.use(cors());
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
@@ -12,7 +15,34 @@ server.engine("hbs", engine({ extname: ".hbs" }));
 server.set("view engine", "hbs");
 server.set("views", "./pages");
 
-server.get("/", (req, res) => {});
+server.get("/", (req, res) => {
+  db.all("SELECT * FROM post;", (err, rows) => {
+    console.log(err);
+
+    res.render("index", { posts: rows });
+  });
+});
+
+server.get("/add-post", (req, res) => {
+  res.render("add-post");
+});
+
+server.post("/new-post", (req, res) => {
+  const { nickname, content } = req.body;
+  const sql = "INSERT INTO post(nickname, content, date) VALUES (?,?,?);";
+  const params = [nickname, content, new Date().toLocaleString()];
+  db.run(sql, params, (err) => {
+    console.log(err);
+    res.redirect("/");
+  });
+});
+
+server.get("/delete/:id", (req, res) => {
+  db.run("DELETE FROM post WHERE id=?", [req.params.id], (err) => {
+    console.log(err);
+    res.redirect("/");
+  });
+});
 const PORT = process.env.PORT || 1112;
 server.listen(PORT, () => console.log(`server running on port  ${PORT}`));
 
