dc94c9e2700a4ef7e9901904744839ba1b1056e2
DilshodbekNodeJs <DilshodbekNodejs@gmail.com>
2023-03-25 09:37:58 +0500

    6-dars

--- server.ts
+++ server.ts
@@ -1,8 +1,10 @@
 import express, { Express } from "express";
 import cors from "cors";
 import { engine } from "express-handlebars";
+import sqlite3 from "sqlite3";
 
 const server: Express = express();
+const db = new sqlite3.Database("./database.db");
 
 server.use(cors());
 server.use(express.json());
@@ -12,7 +14,9 @@ server.engine("hbs", engine({ extname: ".hbs" }));
 server.set("view engine", "hbs");
 server.set("views", "./pages");
 
-server.get("/", (req, res) => {});
+server.get("/", (req, res) => {
+    res.render('index')
+});
 const PORT = process.env.PORT || 1112;
 server.listen(PORT, () => console.log(`server running on port  ${PORT}`));
 
